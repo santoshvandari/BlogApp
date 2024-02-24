3ed9d51a1cd106702ff2a563fb6a038866232bdd
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-24 09:37:29 +0545

    Intial

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +116,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Adding the Media DIR 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media/'
