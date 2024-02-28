c26bd1dde42abe69828c747f4fd9424ae813b7d8
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-28 18:41:34 +0545

    Initial

--- Home/urls.py
+++ Home/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from Home import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('home/',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('team/',views.team,name='team'),
 ]
