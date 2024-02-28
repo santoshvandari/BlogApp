c5f711d8c2c3182be4b0d871238951872cc40467
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-28 18:41:59 +0545

    Initial

--- Home/views.py
+++ Home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def team(request):
+    return render(request,'team.html')
