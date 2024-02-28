49002c9a23097914f0abae89673931224c78ed7f
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-28 18:57:47 +0545

    Initial

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from froala_editor.fields import FroalaField
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=1000)
+    content= FroalaField()
+
+
+    def __str__(self):
+        return self.title
