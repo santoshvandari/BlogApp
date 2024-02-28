e3931877d8e97fbf437ffd269793ad4917ddb157
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-28 19:20:14 +0545

    Initial

--- Home/models.py
+++ Home/models.py
@@ -5,7 +5,10 @@ from froala_editor.fields import FroalaField
 class Blog(models.Model):
     title = models.CharField(max_length=1000)
     content= FroalaField()
-
+    slug=models.SlugField(max_length=1000)
+    image=models.ImageField(upload_to='blog/')
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- Home/urls.py
+++ Home/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from Home import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('home/',views.index,name='index'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('team/',views.team,name='team'),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
