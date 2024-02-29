e84a3ad125cc07decb6bfd37a501168cc3793d9d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-29 10:29:00 +0545

    Initial

--- Home/urls.py
+++ Home/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from Home import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 
@@ -12,8 +10,4 @@ urlpatterns = [
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('team/',views.team,name='team'),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path,include
 from froala_editor import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Home.urls')),
     path('froala_editor/',include('froala_editor.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
